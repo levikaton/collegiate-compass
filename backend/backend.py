9c04bd066051b980f4965ab60b6c0db58f31c6b3
AneesAla <jezar1231@gmail.com>
2024-01-27 12:11:13 -0600

    Updated backend
    added ACT and SAT scores. Degree names, types, and writes to a JSON file

--- backend/backend.py
+++ backend/backend.py
@@ -1,6 +1,7 @@
 import requests
+import json
 
-def search_school(api_key, school_name):
+def search_schools(api_key, school_name):
     base_url = 'https://api.data.gov/ed/collegescorecard/v1/'
     endpoint = 'schools'
     
@@ -16,41 +17,63 @@ def search_school(api_key, school_name):
         results = data.get('results')
 
         if results:
-            location_info = results[0].get('location')
-            if location_info:
-                latitude = location_info.get('lat')
-                longitude = location_info.get('lon')
-                print(f"Latitude: {latitude}")
-                print(f"Longitude: {longitude}")
+            schools_info = []
 
-            latest_info = results[0].get('latest')
-            if latest_info:
-                school_info = latest_info.get('school')
-                if school_info:
-                    zip_code = school_info.get('zip')
-                    city = school_info.get('city')
-                    state = school_info.get('state')
-                    address = school_info.get('address')
-                    school_url = school_info.get('school_url')
-                    online_only = school_info.get('online_only')
+            for result in results:
+                school_info = {}
+                
+                location_info = result.get('location')
+                if location_info:
+                    school_info['latitude'] = location_info.get('lat')
+                    school_info['longitude'] = location_info.get('lon')
 
-                    print(f"Zip Code: {zip_code}")
-                    print(f"City: {city}")
-                    print(f"State: {state}")
-                    print(f"Address: {address}")
-                    print(f"School URL: {school_url}")
-                    print(f"Online Only: {'Yes' if online_only == 1 else 'No'}")
+                latest_info = result.get('latest')
+                if latest_info:
+                    school_info['name'] = latest_info.get('school', {}).get('name', 'N/A')
+                    school_info['zip_code'] = latest_info.get('school', {}).get('zip', 'N/A')
+                    school_info['city'] = latest_info.get('school', {}).get('city', 'N/A')
+                    school_info['state'] = latest_info.get('school', {}).get('state', 'N/A')
+                    school_info['address'] = latest_info.get('school', {}).get('address', 'N/A')
+                    school_info['school_url'] = latest_info.get('school', {}).get('school_url', 'N/A')
+                    school_info['online_only'] = latest_info.get('school', {}).get('online_only', 'N/A')
 
                     cost_info = latest_info.get('cost')
                     if cost_info:
-                        booksupply_cost = cost_info.get('booksupply')
-                        tuition_info = cost_info.get('tuition')
-                        in_state_tuition = tuition_info.get('in_state')
-                        out_of_state_tuition = tuition_info.get('out_of_state')
+                        school_info['booksupply_cost'] = cost_info.get('booksupply', 'N/A')
+                        tuition_info = cost_info.get('tuition', {})
+                        school_info['in_state_tuition'] = tuition_info.get('in_state', 'N/A')
+                        school_info['out_of_state_tuition'] = tuition_info.get('out_of_state', 'N/A')
 
-                        print(f"Book and Supply Cost: ${booksupply_cost}")
-                        print(f"In-State Tuition: ${in_state_tuition}")
-                        print(f"Out-of-State Tuition: ${out_of_state_tuition}")
+                    admissions_info = latest_info.get('admissions')
+                    if admissions_info:
+                        act_scores_info = admissions_info.get('act_scores', {}).get('midpoint', {})
+                        school_info['act_midpoint_math'] = act_scores_info.get('math', 'N/A')
+                        school_info['act_midpoint_english'] = act_scores_info.get('english', 'N/A')
+                        school_info['act_midpoint_writing'] = act_scores_info.get('writing', 'N/A')
+                        school_info['act_midpoint_cumulative'] = act_scores_info.get('cumulative', 'N/A')
+
+                        sat_scores_info = admissions_info.get('sat_scores', {}).get('midpoint', {})
+                        school_info['sat_midpoint_math'] = sat_scores_info.get('math', 'N/A')
+                        school_info['sat_midpoint_writing'] = sat_scores_info.get('writing', 'N/A')
+                        school_info['sat_midpoint_critical_reading'] = sat_scores_info.get('critical_reading', 'N/A')
+
+                    programs_info = latest_info.get('programs')
+                    if programs_info:
+                        cip_4_digit_info = programs_info.get('cip_4_digit', [])
+                        school_info['programs'] = []
+                        for program in cip_4_digit_info:
+                            credential_info = program.get('credential')
+                            if credential_info and credential_info.get('level') == 3:
+                                program_info = {
+                                    'title': program.get('title', 'N/A'),
+                                    'credential_level': credential_info.get('title', 'N/A')
+                                }
+                                school_info['programs'].append(program_info)
+
+                schools_info.append(school_info)
+
+            with open('schools_data.json', 'w') as json_file:
+                json.dump(schools_info, json_file, indent=2)
 
         else:
             print(f"No results found for '{school_name}'.")
@@ -62,7 +85,7 @@ def main():
 
     school_name = input("Enter the name of the school: ")
 
-    search_school(api_key, school_name)
+    search_schools(api_key, school_name)
 
 if __name__ == "__main__":
     main()
